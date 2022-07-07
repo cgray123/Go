e6f968ef378d430c24f746bb8fdfdc4dd4f14634
cgray123 <52083757+cgray123@users.noreply.github.com>
2022-07-07 15:57:10 -0400

    Update main.go

--- July7/main.go
+++ July7/main.go
@@ -6,11 +6,18 @@ import (
 )
 
 func main() {
+	/*
 	var mapOfPeople map[int]sub.People
 	p1 := sub.People{Name: "Joe", Age: 18}
 	p2 := sub.People{Name: "Bob", Age: 28}
 	p3 := sub.People{Name: "Steve", Age: 40}
 	mapOfPeople = map[int]sub.People{1: p1, 2: p2, 3: p3}
 	fmt.Println(mapOfPeople)
+	*/
 	//sub.MapSearch()
+	/*
+	var m = map[string]int{"orange": 5, "apple": 7, "pear": 3, "strawberry": 9}
+	sub.MapSort(m)
+	*/
+	sub.Csummary()
 }
