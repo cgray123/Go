789bbef75ed8fd95bca95166f3fd59188eee9d67
cgray123 <52083757+cgray123@users.noreply.github.com>
2022-07-07 12:23:58 -0400

    Create main.go

---
+++ July7/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"mymod/sub"
+)
+
+func main() {
+	var mapOfPeople map[int]sub.People
+	p1 := sub.People{Name: "Joe", Age: 18}
+	p2 := sub.People{Name: "Bob", Age: 28}
+	p3 := sub.People{Name: "Steve", Age: 40}
+	mapOfPeople = map[int]sub.People{1: p1, 2: p2, 3: p3}
+	fmt.Println(mapOfPeople)
+}
