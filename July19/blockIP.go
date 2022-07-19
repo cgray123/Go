c9aa3ce49b14aafaa9096bfd0570f727b1e0bc0e
cgray123 <52083757+cgray123@users.noreply.github.com>
2022-07-19 13:35:40 -0400

    Update blockIP.go

--- July19/blockIP.go
+++ July19/blockIP.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -11,7 +12,9 @@ import (
 var s = new(store)
 
 func main() {
+
 	r := mux.NewRouter()
+
 	r.HandleFunc("/", HomeHandler).Methods("GET")
 	http.Handle("/", r)
 	http.ListenAndServe("127.0.0.1:8000", nil)
@@ -22,12 +25,14 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 
 	if p == "127.0.0.1:2658" {
 		fmt.Fprint(w, "IP Blocked")
-	} else if s.getData() == "" {
+	} else if len(s.getData()) == 0 {
 		t := time.Now().Format(time.ANSIC)
 		s.storeData(t)
 		fmt.Fprint(w, "IP: "+p+", first accessed: "+t)
 	} else {
-		fmt.Fprint(w, "IP: "+p+", last accessed: "+s.getData())
+		fmt.Fprintln(w, "IP: "+p+", last accessed: "+s.getData()[len(s.getData())-2])
+		fmt.Fprint(w, "All times accessed: ")
+		fmt.Fprint(w, strings.Join(s.getData(), ", "))
 		t := time.Now().Format(time.ANSIC)
 		s.storeData(t)
 	}
@@ -35,12 +40,12 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 type store struct {
-	data string
+	data []string
 }
 
 func (a *store) storeData(s string) {
-	a.data = s
+	a.data = append(a.data, s)
 }
-func (a *store) getData() string {
+func (a *store) getData() []string {
 	return a.data
 }
