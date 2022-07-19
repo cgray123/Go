b23fa256ff6bc58cb8ead1c4985a79b9dab54a25
cgray123 <52083757+cgray123@users.noreply.github.com>
2022-07-19 13:26:32 -0400

    Update blockIP.go

--- July19/blockIP.go
+++ July19/blockIP.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 )
+
+var s = new(store)
 
 func main() {
 	r := mux.NewRouter()
@@ -17,11 +19,28 @@ func main() {
 }
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	p := r.RemoteAddr
+
 	if p == "127.0.0.1:2658" {
 		fmt.Fprint(w, "IP Blocked")
+	} else if s.getData() == "" {
+		t := time.Now().Format(time.ANSIC)
+		s.storeData(t)
+		fmt.Fprint(w, "IP: "+p+", first accessed: "+t)
 	} else {
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(p)
+		fmt.Fprint(w, "IP: "+p+", last accessed: "+s.getData())
+		t := time.Now().Format(time.ANSIC)
+		s.storeData(t)
 	}
 
+}
+
+type store struct {
+	data string
+}
+
+func (a *store) storeData(s string) {
+	a.data = s
+}
+func (a *store) getData() string {
+	return a.data
 }
