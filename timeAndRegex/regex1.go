fe925597d1261c1763588765dacbae31094a5cd4
cgray123 <52083757+cgray123@users.noreply.github.com>
2022-07-29 10:09:14 -0400

    Create regex1.go

---
+++ timeAndRegex/regex1.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+)
+
+func main() {
+	str := "Hello I am 20 years 10 month and 20 days old cat cot  my DOB is 2000-11-05 my telephone number is +11235551234 my IP address is 192.166.255 192.170.1"
+
+	re1 := regexp.MustCompile(`192\.1(6[0-9]\.(2[0-5][0-5]|1[0-9][0-9]|[1-9][0-9]|[1-9])|70\.1)`)
+	re3 := regexp.MustCompile("c([a-z]+)t")
+
+	mat := re3.FindAllString(str, -1)
+	for _, ind := range mat {
+		fmt.Println(ind)
+	}
+	mat2 := re1.FindAllString(str, -1)
+	for _, ind := range mat2 {
+		fmt.Println(ind)
+	}
+}
