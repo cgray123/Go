e9c633860aacd97167fb9a4a9fd4e3d5409ef450
cgray123 <52083757+cgray123@users.noreply.github.com>
2022-07-29 12:10:49 -0400

    Update regex1.go

--- timeAndRegex/regex1.go
+++ timeAndRegex/regex1.go
@@ -7,8 +7,9 @@ import (
 
 func main() {
 	str := "Hello I am 20 years 10 month and 20 days old cat cot  my DOB is 2000-11-05 my telephone number is +11235551234 my IP address is 192.166.255 192.170.1"
-
+	//regex pattern for finding a ip in the range 192.160.1 to 192.170.1
 	re1 := regexp.MustCompile(`192\.1(6[0-9]\.(2[0-5][0-5]|1[0-9][0-9]|[1-9][0-9]|[1-9])|70\.1)`)
+	//regex pattern for finding words that start with c and end with t
 	re3 := regexp.MustCompile("c([a-z]+)t")
 
 	mat := re3.FindAllString(str, -1)
